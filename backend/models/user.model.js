192f7941070be817aaa588dec6c181e1a6ca5c50
iAmNsengi <iamnsengi@icloud.com>
2022-12-03 17:50:13 +0200

    ft: update User model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    profilePicture: { type: String, default: "" },
+    bannerImg: { type: String, default: "" },
+    headline: { type: String, default: "LinkedOut User" },
   },
   { timestamps: true }
 );
