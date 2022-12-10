9e5e7b11d4b3b37036c7f34e2274da29730d7be8
iAmNsengi <iamnsengi@icloud.com>
2022-12-10 08:38:58 +0200

    ft: add mailtrap config

---
+++ test.mailtrap.js
@@ -0,0 +1,27 @@
+const { MailtrapClient } = require("mailtrap");
+
+const TOKEN = "5d90f3f7fe274649aa54f682c0637803";
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "nsengitech@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
