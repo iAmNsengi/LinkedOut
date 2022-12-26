aa1b22787d732c13852f046f854ae6fcd2d57e75
iAmNsengi <iamnsengi@icloud.com>
2022-12-26 15:27:30 +0200

    ft: find one post endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,12 +4,16 @@ import {
   createPost,
   deletePost,
   getFeedPosts,
+  getPostById,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectedRoute, getFeedPosts);
 router.post("/create", protectedRoute, createPost);
+
+router.get("/:id", protectedRoute, getPostById);
+
 router.delete("/delete/:id", protectedRoute, deletePost);
 
 export default router;
