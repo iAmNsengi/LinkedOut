5822415910f6adfad66abb4ee47ff5a64d3df511
iAmNsengi <iamnsengi@icloud.com>
2022-12-28 17:47:34 +0200

    ft: send comment notification to email

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,7 @@
+import { createCommentNotificationEmailTemplate } from "../emails/emailTemplates.js";
 import cloudinary from "../lib/cloudinary.js";
+import { mailtrapClient, sender } from "../lib/mailtrap.js";
+import Notification from "../models/notification.model.js";
 import Post from "../models/post.model.js";
 
 export const getFeedPosts = async (req, res) => {
@@ -101,11 +104,65 @@ export const createComment = async (req, res) => {
       },
       { new: true }
     ).populate("author", "name email username headline profilePicture");
+
+    // send the notification if we are not the owner of the post and the comment
+    if (post.author.toString() !== req.user._id.toString()) {
+      const newNotification = new Notification({
+        recipient: post.author,
+        type: "comment",
+        relatedUser: req.user._id,
+        relatedPost: id,
+      });
+
+      await newNotification.save();
+
+      // send email of notification
+      try {
+        const postUrl = process.env.CLIENT_URL + "/post/" + id;
+        await sendCommentNotification(
+          post.author.email,
+          post.author.name,
+          req.user.name,
+          postUrl,
+          content
+        );
+        console.log("Comment notification email sent successfully");
+      } catch (error) {
+        console.error("Failed to send email, ", error.message);
+      }
+    }
+
     return res.status(200).json(postWithComment);
   } catch (error) {
     console.error("Error in getPostById ", error);
     return res.status(500).json({
       message: `An internal server error occurred, ${error.message}`,
     });
+  }
+};
+
+export const sendCommentNotification = async (
+  recipientEmail,
+  recipientName,
+  commentorName,
+  postUrl,
+  commentContent
+) => {
+  try {
+    const recipient = [{ email }];
+    const res = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "New comment on your post",
+      html: createCommentNotificationEmailTemplate(
+        recipientName,
+        commentorName,
+        postUrl,
+        commentContent
+      ),
+      category: "comment_notification",
+    });
+  } catch (error) {
+    throw error;
   }
 };
