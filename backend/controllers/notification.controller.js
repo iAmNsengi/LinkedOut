89989dec66744998c7883a7320325e1b9076a9de
iAmNsengi <iamnsengi@icloud.com>
2023-01-12 20:48:03 +0200

    ft: get notifications

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,38 @@
+import Notification from "../models/notification.model.js";
+
+export const getUserNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find({
+      recipient: req.user._id,
+    })
+      .sort({ createdAt: -1 })
+      .populate("relatedUser", "name username profilePicture");
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.error("Error in getUserNotifications, ", error.message);
+    return res
+      .status(500)
+      .json({ message: `Internal server error, ${error.message}` });
+  }
+};
+
+export const markNotificationAsRead = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const notification = await Notification.findById(id);
+    if (!notification)
+      return res.status(400).json({ message: "Notification not found" });
+
+    notification.read = true;
+    await notification.save();
+    return res
+      .status(200)
+      .json({ message: "Notification updated successfully", notification });
+  } catch (error) {
+    console.error("Error in mark notification as read, ", error.message);
+    return res
+      .status(500)
+      .json({ message: `Internal server error, ${error.message}` });
+  }
+};
---
+++ backend/routes/notification.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectedRoute, getUserNotifications);
+
+router.put("/:id/read", protectedRoute, markNotificationAsRead);
+router.delete("/:id", protectedRoute, deleteNotification);
+
+export default router;
