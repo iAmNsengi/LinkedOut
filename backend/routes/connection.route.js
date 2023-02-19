f8d4d6eceed9b2f01576b9d7f3f8a2d786171a21
iAmNsengi <iamnsengi@icloud.com>
2023-02-19 09:33:39 +0200

    ft: add connection routes

--- backend/routes/connection.route.js
+++ backend/routes/connection.route.js
@@ -1,5 +1,15 @@
 import express from "express";
+import { protectedRoute } from "../middlewares/auth.middleware";
 
 const router = express.Router();
+
+router.post('/request/":userId', protectedRoute, sendConnectionRequest);
+router.put('/request/":requestId', protectedRoute, acceptConnectionRequest);
+router.put('/request/":requestId', protectedRoute, rejectConnectionRequest);
+
+router.get("/requests", protectedRoute, getConnectionRequests);
+router.get("/", protectedRoute, getUserConnections);
+router.delete("/:userId", protectedRoute, removeConnection);
+router.get("/status/:userId", protectedRoute, getConnectionStatus);
 
 export default router;
