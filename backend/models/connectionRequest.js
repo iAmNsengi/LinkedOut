55037432fc56ba0cc8e84b3e61716bc790b056f5
iAmNsengi <iamnsengi@icloud.com>
2023-02-19 09:28:39 +0200

    ft: add connection request schema

---
+++ backend/models/connectionRequest.js
@@ -0,0 +1,29 @@
+import mongoose, { connection } from "mongoose";
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const ConnectionRequest = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+
+return ConnectionRequest;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import connectionRoutes from "./routes/connection.route.js";
+
 
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -21,6 +23,8 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/notification", notificationRoutes);
+app.use("/api/v1/connections", connectionRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
